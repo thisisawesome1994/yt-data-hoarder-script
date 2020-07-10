c8710f6dd3d73dd3175066bf4aeade4ab0730504
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2020-07-10 23:06:29 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,7 @@
+import time
+import os
+import subprocess
+subprocess.run(["youtube-dl.exe", "-U"])
+subprocess.run(["youtube-dl.exe", "-i", "https://www.youtube.com/user/detelegraaf"])
+time.sleep(28800)
+os.startfile("script.exe")
